4e1315191b7ba2e4acc0ae5caf358fe9b8b3cc90
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2022-09-06 19:41:38 -0300

    Add files via upload

--- utils/loader.py
+++ utils/loader.py
@@ -1,29 +1,57 @@
 import discord,os
+
 from discord.ext import commands
 from .configs import configData
 
-def __run__(token):
+class loadcogs():
 
-    client = commands.Bot(
+    def __init__(self, bot):
 
-    command_prefix = configData['prefix'],
+        self.bot = bot
 
-    help_command = None,
+        pass
 
-    intents = discord.Intents.all(),
+    def load(self):
 
-    case_insensitive = True)
+        for filename in os.listdir('./plugins'):
 
-    for filename in os.listdir('./plugins'):
+            if filename.endswith('.py'):
 
-        if filename.endswith('.py'):
+                self.bot.load_extension('plugins.{0}'.format(filename[:-3]))
 
-            client.load_extension('plugins.{0}'.format(filename[:-3]))
+        for filename in os.listdir('./commands'):
 
-    for filename in os.listdir('./commands'):
+            if filename.endswith('.py'):
 
-        if filename.endswith('.py'):
+                self.bot.load_extension('commands.{0}'.format(filename[:-3]))
 
-            client.load_extension('commands.{0}'.format(filename[:-3]))
+class Client(commands.Bot):
 
-    client.run(token)
+    def __init__(self, token):
+
+        self.token = token
+        
+        super().__init__(
+
+            command_prefix = configData['prefix'],
+
+            help_command = None,
+
+            intents = discord.Intents.all(),
+
+            case_insensitive = True
+        )
+
+    async def on_connect(self):
+
+        await self.change_presence(activity=discord.Game(name="/help"))
+
+        print(f'Eu estou online como {self.user}')
+
+        print(discord.__version__)
+
+    def __run__(self):
+
+        loadcogs(self).load()
+
+        self.run(self.token)
